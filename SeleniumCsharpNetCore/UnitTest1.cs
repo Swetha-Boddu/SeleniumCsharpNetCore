c1244d6b495a1c62e38c57fbbf1fe2d68cb6e0d3
Swetha-Boddu <swethanani124@gmail.com>
2022-07-02 14:39:36 +0530

    Removed the console.writeline code

--- SeleniumCsharpNetCore/UnitTest1.cs
+++ SeleniumCsharpNetCore/UnitTest1.cs
@@ -27,9 +27,7 @@ namespace SeleniumCsharpNetCore
             Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
 
             // CustomControl control = new CustomControl();
-            CustomControl.combobox("ContentPlaceHolder1_AllMealsCombo", "Almond");
-
-            Console.WriteLine("Test1ContentPlaceHolder1_AllMealsCombo");
+            CustomControl.combobox("ContentPlaceHolder1_AllMealsCombo", "Almond");            
             Assert.Pass();
         }
     }
